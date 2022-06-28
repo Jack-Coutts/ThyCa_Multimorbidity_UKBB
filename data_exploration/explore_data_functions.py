c6b69236a35e1440f8338e468329e0b419e7840e
jackcoutts <jackwgcoutts@gmail.com>
2022-06-28 23:50:39 +0100

    Additional functions.

--- data_exploration/explore_data_functions.py
+++ data_exploration/explore_data_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def find_col_frequency(tsv_file, col_name):
+def find_col_frequency(tsv_file, colname):
 
     data = pd.read_csv(tsv_file, sep='\t', header=0, index_col=0)
     participant_num = len(data.index)
@@ -9,11 +9,16 @@ def find_col_frequency(tsv_file, col_name):
     pass
 
 
-def all_col_frequency():
+def all_col_frequency(tsv_file):
 
     pass
 
 
-def count_col_yes():  # Number of participants with 1 in column e.g. num of participants with ThyCa
+def count_col_yes(tsv_file, colname):  # Number of participants with 1 in column e.g. num of participants with ThyCa
+
+    pass
+
+
+def phecode_conversion():  # Convert phecode to disease name or medication name
 
     pass
