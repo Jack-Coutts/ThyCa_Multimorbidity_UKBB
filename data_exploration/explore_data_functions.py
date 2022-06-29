dcc09726f5cd6158da4b0e4bf02488d030c06965
jackcoutts <jackwgcoutts@gmail.com>
2022-06-29 11:09:45 +0100

    Column value frequency function.

--- data_exploration/explore_data_functions.py
+++ data_exploration/explore_data_functions.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
 
-def find_col_frequency(tsv_file, colname):
+def find_col_frequency(tsv_file, colname, target_value):  # Frequency of column value
 
-    data = pd.read_csv(tsv_file, sep='\t', header=0, index_col=0)
-    participant_num = len(data.index)
+    data = pd.read_csv(tsv_file, sep='\t', header=0, index_col=0)  # Read in tsv
+    participant_num = len(data.index)  # Number of rows/participants
+    col_of_interest = list(data[colname])  # Turn target column into list
+    count = col_of_interest.count(target_value)  # Count number of target column value (e.g. 1) in list
+    frequency = count/participant_num  # Find frequency of target value
 
-    pass
+    return count, frequency  # Return value count and frequency
 
 
 def all_col_frequency(tsv_file):
@@ -14,11 +17,8 @@ def all_col_frequency(tsv_file):
     pass
 
 
-def count_col_yes(tsv_file, colname):  # Number of participants with 1 in column e.g. num of participants with ThyCa
-
-    pass
-
 
 def phecode_conversion():  # Convert phecode to disease name or medication name
 
     pass
+
