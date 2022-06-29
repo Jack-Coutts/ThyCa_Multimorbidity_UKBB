d8a483907679ff290cc7132e7cd1c0c460fc2bd0
jackcoutts <jackwgcoutts@gmail.com>
2022-06-29 12:26:24 +0100

    All col frequency function.

--- data_exploration/explore_data_functions.py
+++ data_exploration/explore_data_functions.py
@@ -22,13 +22,13 @@ def find_col_frequency(tsv_file, colname, target_value):  # Frequency of column 
 def all_col_frequency(tsv_file, target_value):  # Return tsv with frequency of all columns
 
     data = pd.read_csv(tsv_file, sep='\t', header=0, index_col=0)  # Read in tsv
-
-    out_df_rows = [i.count(target_value) for i in [list(data[i]) for i in data.columns]]
-
-    out_df = pd.DataFrame(out_df_rows, columns=data.columns)
+    row_count = len(data.index)  # Row count
+    # List of column frequencies
+    out_df_rows = [[i.count(target_value)/row_count] for i in [list(data[i]) for i in data.columns]]
+    # Dataframe with column name as index and freq as column
+    out_df = pd.DataFrame(out_df_rows, columns=['Frequency'], index=data.columns)
 
     return out_df
-
 
 
 def phecode_conversion():  # Convert phecode to disease name or medication name
