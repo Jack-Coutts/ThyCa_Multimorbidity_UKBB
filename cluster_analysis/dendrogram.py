5c0c78c6bbd81e07908d93cf6c71606172d32cb3
jackcoutts <jackwgcoutts@gmail.com>
2022-07-01 13:38:09 +0100

    Added dendrogram output files from apocrita.

--- cluster_analysis/dendrogram.py
+++ cluster_analysis/dendrogram.py
@@ -1,11 +1,11 @@
 
-from cluster_analysis.cluster_dendrogram_functions import *
+from cluster_dendrogram_functions import *
 
 # Read in data and transpose
 data = tsv_to_df('/data/home/bt211037/dissertation/input/raw_data/phecodes_clean_2021.txt', 0, True)  # Index = userId
 
 # Plot dendrogram using complete linkage
-plot_dendro('UK Biobank Disease Clusters', data, 'complete', 'jaccard', 'complete_lnk_dendro.png')
+plot_dendro('UK Biobank Disease Clusters', data, 'complete', 'jaccard', 'apocrita_out_files/complete_lnk_dendro.png')
 
 # Plot dendrogram using single linkage
-plot_dendro('UK Biobank Disease Clusters', data, 'complete', 'jaccard', 'single_lnk_dendro.png')
+plot_dendro('UK Biobank Disease Clusters', data, 'complete', 'jaccard', 'apocrita_out_files/single_lnk_dendro.png')
