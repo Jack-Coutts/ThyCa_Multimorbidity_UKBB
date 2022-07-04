a2f06e207d331974dfcb60132486424d6714d939
jackcoutts <jackwgcoutts@gmail.com>
2022-07-04 13:40:03 +0100

    Edited spelling mistakes in arm analysis file.

--- association_rule_mining/arm_analysis.py
+++ association_rule_mining/arm_analysis.py
@@ -1,13 +1,13 @@
 from arm_functions import *
 
 # Read in cluster tsv - userId as index
-data = tsv_to_df('/data/home/bt211037/dissertation/cluster_analysis/ThyCa_clutser.tsv', 0)
+data = tsv_to_df('/data/home/bt211037/dissertation/cluster_analysis/ThyCa_cluster.tsv', 0)
 
 # Create a list of smaller dataframes from the larger one
 small_dfs = split_df(data, 'p193', 20)
 
 # Run analysis of each dataframe and save association riles involving ThyCa
 results = multi_df_ARM(0.0001, 'confidence', 0.001, 'p193',
-                       '/data/home/bt211037/dissertation/arm_analysis/ThyCa_arm_results.tsv',
+                       '/data/home/bt211037/dissertation/association_rule_mining/ThyCa_arm_results.tsv',
                        small_dfs)
 
