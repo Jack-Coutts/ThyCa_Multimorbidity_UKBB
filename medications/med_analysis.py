c97f653c548ff7b2944a0a1ffdc20828c1d06c03
jackcoutts <jackwgcoutts@gmail.com>
2022-07-04 15:28:06 +0100

    Fixed indexing error and incorrect use of function in med_analysis.py.

--- medications/med_analysis.py
+++ medications/med_analysis.py
@@ -1,7 +1,7 @@
 from med_functions import *
 
 # Read in disease data
-data = tsv_to_df('/data/home/bt211037/dissertation/input/raw_data/phecodes_clean_2021.txt')
+data = tsv_to_df('/data/home/bt211037/dissertation/input/raw_data/phecodes_clean_2021.txt',0)
 
 # Read in medication data
 mdata = tsv_to_df('/data/home/bt211037/dissertation/input/raw_data/full_med_20003_phec_nona_over20.txt', 0)
@@ -10,10 +10,10 @@ mdata = tsv_to_df('/data/home/bt211037/dissertation/input/raw_data/full_med_2000
 med_ids = mdata.index.tolist()
 
 # Retain disease data participants only if there is mediction data
-data = data.loc[data['userId'].isin(med_ids)]
+data = data.loc[data.index.isin(med_ids)]
 
 # Add thyroxine medication column to disease dataframe
-add_col(data, 'thyroxine medication', mdata, 'm1140874852', 'm1140884516', 'm1140910814', 'm1141191044')
+add_col(data, 1, 'thyroxine medication', mdata, 'm1140874852', 'm1140884516', 'm1140910814', 'm1141191044')
 
 # Split the disease data into smaller dataframes
 small_dfs = split_df(data, 'thyroxine medication', 40)
